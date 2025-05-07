15bbbf0e8ca1d3e78bcfecef647c35b6dff57bc6
febo <febo@anza.xyz>
2025-05-07 21:02:58 +0100

    Add invoke instruction helper

--- sdk/pinocchio/src/cpi.rs
+++ sdk/pinocchio/src/cpi.rs
@@ -4,7 +4,7 @@ use core::{mem::MaybeUninit, ops::Deref};
 
 use crate::{
     account_info::{AccountInfo, BorrowState},
-    instruction::{Account, Instruction, Signer},
+    instruction::{Account, AccountMeta, Instruction, Signer},
     program_error::ProgramError,
     pubkey::Pubkey,
     ProgramResult,
@@ -13,7 +13,26 @@ use crate::{
 /// Maximum number of accounts that can be passed to a cross-program invocation.
 pub const MAX_CPI_ACCOUNTS: usize = 64;
 
+/// A constant to indicate that an account is read-only when passed as
+/// an account priviledge.
+pub const READONLY: bool = false;
+
+/// A constant to indicate that an account is writable when passed as
+/// an account priviledge.
+pub const WRITABLE: bool = true;
+
+/// A type representing the privileges of an account for
+/// `invoke_instruction` and `invoke_instruction_signed`.
+///
+/// The first element of the tuple indicates whether the account is
+/// writable or not, and the second element indicates whether the
+/// account is a signer or not.
+pub type Priviledge = (bool, bool);
+
 /// Invoke a cross-program instruction.
+///
+/// This function is a convenience wrapper around [`invoke_signed`] that
+/// passes an empty signer seeds slice.
 ///
 /// # Important
 ///
@@ -29,6 +48,9 @@ pub fn invoke<const ACCOUNTS: usize>(
 
 /// Invoke a cross-program instruction from a slice of `AccountInfo`s.
 ///
+/// This function is a convenience wrapper around [`slice_invoke_signed`]
+/// that passes an empty signer seeds slice.
+///
 /// # Important
 ///
 /// The accounts on the `account_infos` slice must be in the same order as the
@@ -39,6 +61,21 @@ pub fn slice_invoke(instruction: &Instruction, account_infos: &[&AccountInfo]) -
 }
 
 /// Invoke a cross-program instruction with signatures.
+///
+/// This function performs validation of the `account_infos` slice to ensure that:
+///   1. The accounts match the expected accounts in the instruction, i.e., their
+///      `Pubkey` matches the `pubkey` in the `AccountMeta`.
+///   2. The borrow state of the accounts is compatible with the mutability of the
+///      accounts in the instruction.
+///
+/// This validation is done to ensure that the borrow checker rules are followed,
+/// consuming CUs in the process. There are two alternatives to this function that
+/// have lower CU consumption:
+///   * `invoke_instruction_signed` - does not perform `Pubkey` validation since
+///      it creates each `AccountMeta` from the `Account` directly.
+///   * `invoke_signed_unchecked` - does not perform any validation. This should
+///      only be used when the caller is sure that the borrow checker rules are
+///      followed.
 ///
 /// # Important
 ///
@@ -55,16 +92,19 @@ pub fn invoke_signed<const ACCOUNTS: usize>(
 
     account_infos
         .iter()
+        .zip(instruction.accounts.iter())
         .enumerate()
-        .try_for_each(|(index, account_info)| {
+        .try_for_each(|(index, (account_info, account_meta))| {
+            // In order to check whether the borrow state is compatible
+            // with the invocation, we need to check that we have the
+            // correct account info and meta pair
+            if account_info.key() != account_meta.pubkey {
+                return Err(ProgramError::InvalidArgument);
+            }
+
             // Check whether all account infos can be safely borrowed according
             // to their mutability on the instruction or not.
-            //
-            // This is a best effort approach since it is cheaper than matching each
-            // account info with their account meta and tracking duplicates, but it
-            // is pessimistic since it might sometimes require that read-only accounts
-            // on the cross-program invocation to be mutably borrow "friendly".
-            let state = if account_info.is_writable() {
+            let state = if account_meta.is_writable {
                 BorrowState::Borrowed
             } else {
                 BorrowState::MutablyBorrowed
@@ -79,15 +119,13 @@ pub fn invoke_signed<const ACCOUNTS: usize>(
                 accounts
                     .get_unchecked_mut(index)
                     .write(Account::from(*account_info));
-                //.write(Account::from_account_info(account_info));
             }
 
             Ok(())
         })?;
 
     // SAFETY: At this point it is guaranteed that all account infos are
-    // borrowable according to their mutability on the instruction, which
-    // in the worst case is more than what is needed.
+    // borrowable according to their mutability on the instruction.
     unsafe {
         invoke_signed_unchecked(
             instruction,
@@ -101,6 +139,18 @@ pub fn invoke_signed<const ACCOUNTS: usize>(
 
 /// Invoke a cross-program instruction with signatures from a slice of
 /// `AccountInfo`s.
+///
+/// This function performs validation of the `account_infos` slice to ensure that:
+///   1. The accounts match the expected accounts in the instruction, i.e., their
+///      `Pubkey` matches the `pubkey` in the `AccountMeta`.
+///   2. The borrow state of the accounts is compatible with the mutability of the
+///      accounts in the instruction.
+///
+/// This validation is done to ensure that the borrow checker rules are followed,
+/// consuming CUs in the process. The `invoke_signed_unchecked` is an alternative
+/// to this function that have lower CU consumption since it does not perform
+/// any validation. This should only be used when the caller is sure that the borrow
+/// checker rules are followed.
 ///
 /// # Important
 ///
@@ -121,16 +171,19 @@ pub fn slice_invoke_signed(
 
     account_infos
         .iter()
+        .zip(instruction.accounts.iter())
         .enumerate()
-        .try_for_each(|(index, account_info)| {
+        .try_for_each(|(index, (account_info, account_meta))| {
+            // In order to check whether the borrow state is compatible
+            // with the invocation, we need to check that we have the
+            // correct account info and meta pair
+            if account_info.key() != account_meta.pubkey {
+                return Err(ProgramError::InvalidArgument);
+            }
+
             // Check whether all account infos can be safely borrowed according
             // to their mutability on the instruction or not.
-            //
-            // This is a best effort approach since it is cheaper than matching each
-            // account info with their account meta and tracking duplicates, but it
-            // is pessimistic since it might sometimes require that read-only accounts
-            // on the cross-program invocation to be mutably borrow "friendly".
-            let state = if account_info.is_writable() {
+            let state = if account_meta.is_writable {
                 BorrowState::Borrowed
             } else {
                 BorrowState::MutablyBorrowed
@@ -168,7 +221,7 @@ pub fn slice_invoke_signed(
 /// Invoke a cross-program instruction but don't enforce Rust's aliasing rules.
 ///
 /// This function does not check that [`Account`]s are properly borrowable.
-/// Those checks consume CPU cycles that this function avoids.
+/// Those checks consume CUs that this function avoids.
 ///
 /// # Safety
 ///
@@ -199,10 +252,102 @@ pub unsafe fn invoke_signed_unchecked(
     accounts: &[Account],
     signers_seeds: &[Signer],
 ) {
+    invoke_instruction_signed_unchecked(
+        instruction.program_id,
+        instruction.accounts,
+        instruction.data,
+        accounts,
+        signers_seeds,
+    );
+}
+
+/// Invoke a cross-program instruction.
+#[inline(always)]
+pub fn invoke_instruction<const ACCOUNTS: usize>(
+    program_id: &Pubkey,
+    priviledges: &[Priviledge; ACCOUNTS],
+    instruction_data: &[u8],
+    accounts: &[Account; ACCOUNTS],
+) -> ProgramResult {
+    invoke_instruction_signed(program_id, priviledges, instruction_data, accounts, &[])
+}
+
+/// Invoke a cross-program instruction with signatures.
+#[inline]
+pub fn invoke_instruction_signed<const ACCOUNTS: usize>(
+    program_id: &Pubkey,
+    priviledges: &[Priviledge; ACCOUNTS],
+    instruction_data: &[u8],
+    accounts: &[Account; ACCOUNTS],
+    signers_seeds: &[Signer],
+) -> ProgramResult {
+    let mut account_metas = MaybeUninit::<[AccountMeta; ACCOUNTS]>::uninit();
+    let mut account_metas_ptr = account_metas.as_mut_ptr() as *mut AccountMeta;
+
+    accounts
+        .iter()
+        .zip(priviledges.iter())
+        .try_for_each(|(account, priviledge)| {
+            // Check whether all account infos can be safely borrowed according
+            // to their mutability on the instruction or not.
+            if account.is_borrowed(match priviledge.0 {
+                true => BorrowState::Borrowed,
+                false => BorrowState::MutablyBorrowed,
+            }) {
+                return Err(ProgramError::AccountBorrowFailed);
+            }
+
+            // SAFETY: There are `ACCOUNTS` account metas.
+            unsafe {
+                account_metas_ptr.write(AccountMeta::new(
+                    account.key(),
+                    priviledge.0,
+                    priviledge.1,
+                ));
+
+                account_metas_ptr = account_metas_ptr.add(1);
+            }
+
+            Ok(())
+        })?;
+
+    // SAFETY: At this point it is guaranteed that all account infos are
+    // borrowable according to their mutability on the instruction.
+    unsafe {
+        invoke_instruction_signed_unchecked(
+            program_id,
+            account_metas.assume_init_ref(),
+            instruction_data,
+            accounts,
+            signers_seeds,
+        );
+    }
+
+    Ok(())
+}
+
+/// Invoke a cross-program instruction with signatures but don't enforce Rust's
+/// aliasing rules.
+///
+/// This function does not check that [`Account`]s are properly borrowable.
+/// Those checks consume CPU cycles that this function avoids.
+///
+/// # Safety
+///
+/// If any of the writable accounts passed to the callee contain data that is
+/// borrowed within the calling program, and that data is written to by the
+/// callee, then Rust's aliasing rules will be violated and cause undefined
+/// behavior.
+#[inline(always)]
+pub unsafe fn invoke_instruction_signed_unchecked(
+    program_id: &Pubkey,
+    account_metas: &[AccountMeta],
+    instruction_data: &[u8],
+    accounts: &[Account],
+    signers_seeds: &[Signer],
+) {
     #[cfg(target_os = "solana")]
     {
-        use crate::instruction::AccountMeta;
-
         /// An `Instruction` as expected by `sol_invoke_signed_c`.
         ///
         /// DO NOT EXPOSE THIS STRUCT:
@@ -229,11 +374,11 @@ pub unsafe fn invoke_signed_unchecked(
         }
 
         let cpi_instruction = CInstruction {
-            program_id: instruction.program_id,
-            accounts: instruction.accounts.as_ptr(),
-            accounts_len: instruction.accounts.len() as u64,
-            data: instruction.data.as_ptr(),
-            data_len: instruction.data.len() as u64,
+            program_id,
+            accounts: account_metas.as_ptr(),
+            accounts_len: account_metas.len() as u64,
+            data: instruction_data.as_ptr(),
+            data_len: instruction_data.len() as u64,
         };
 
         unsafe {
@@ -248,7 +393,16 @@ pub unsafe fn invoke_signed_unchecked(
     }
 
     #[cfg(not(target_os = "solana"))]
-    core::hint::black_box((instruction, accounts, signers_seeds));
+    {
+        core::hint::black_box((
+            program_id,
+            account_metas,
+            instruction_data,
+            accounts,
+            signers_seeds,
+        ));
+        unreachable!();
+    }
 }
 
 /// Maximum size that can be set using [`set_return_data`].
