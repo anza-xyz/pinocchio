31d5b566c56193780e6e4e615ef49a5767ab32ee
NagaprasadVr <nagaprasadvr246@gmail.com>
2025-08-14 18:38:02 +0530

    feat: scaffold extensions dir

--- programs/token-2022/src/lib.rs
+++ programs/token-2022/src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
 
+pub mod extensions;
 pub mod instructions;
 pub mod state;
 
@@ -14,4 +15,13 @@ fn write_bytes(destination: &mut [MaybeUninit<u8>], source: &[u8]) {
     for (d, s) in destination.iter_mut().zip(source.iter()) {
         d.write(*s);
     }
+}
+
+///
+/// # Safety
+///
+/// This function is unsafe because it transmutes the input data to the output type and return a reference.
+pub unsafe fn from_bytes<T>(data: &[u8]) -> &T {
+    assert_eq!(data.len(), core::mem::size_of::<T>());
+    &*(data.as_ptr() as *const T)
 }
