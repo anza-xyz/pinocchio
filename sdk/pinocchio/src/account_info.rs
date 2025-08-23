42b346d9e4c694659ce551439ec94fc637cb4c4f
Sammy Harris <41593264+stegaBOB@users.noreply.github.com>
2025-08-23 18:17:32 -0500

    Apply suggestion from @febo
    Co-authored-by: Fernando Otero <febo@anza.xyz>

--- sdk/pinocchio/src/account_info.rs
+++ sdk/pinocchio/src/account_info.rs
@@ -497,11 +497,7 @@ impl AccountInfo {
 
         // SAFETY:
         // We are checking if the account data is already borrowed, so we are safe to call
-        unsafe {
-            self.resize_unchecked(new_len)?;
-        }
-
-        Ok(())
+        unsafe { self.resize_unchecked(new_len) }
     }
 
     /// Resizes the account's data and udpates the resize delta without checking if the account is already borrowed.
