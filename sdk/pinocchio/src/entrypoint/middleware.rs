fa62057f117b7ebec2fdf5abfdd9d07044aa89d9
Dean 利迪恩 <10921578+deanmlittle@users.noreply.github.com>
2025-09-05 19:21:27 +0800

    Update sdk/pinocchio/src/entrypoint/middleware.rs
    Co-authored-by: Fernando Otero <febo@anza.xyz>

--- sdk/pinocchio/src/entrypoint/middleware.rs
+++ sdk/pinocchio/src/entrypoint/middleware.rs
@@ -1,7 +1,7 @@
 //! Defines the middleware entrypoint, enabling a hot path to bypass
 //! entrypoint deserialization, ejecting to the cold path on failure.
 #[macro_export]
-macro_rules! middleware_entrypoint {
+macro_rules! middleware_program_entrypoint {
     ($hot:expr, $cold:expr) => {
         $crate::middleware_entrypoint!($hot, $cold, { $crate::MAX_TX_ACCOUNTS });
     };
