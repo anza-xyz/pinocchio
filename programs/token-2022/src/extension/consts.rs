fde4f7a784ea129cd7d534c4ed094679039045e0
zubayr1 <write2zubayr@gmail.com>
2025-09-10 12:55:38 +0200

    add extension files

--- programs/token-2022/src/extension/consts.rs
+++ programs/token-2022/src/extension/consts.rs
@@ -1,2 +1,11 @@
 /// Discriminator for the CpiGuard extension.
 pub const CPI_GUARD_EXTENSION: u8 = 34;
+
+/// Discriminator for the ScaledUiAmount extension.
+pub const SCALED_UI_AMOUNT_EXTENSION: u8 = 35;
+
+/// Discriminator for the Pausable extension.
+pub const PAUSABLE_EXTENSION: u8 = 36;
+
+/// Discriminator for the DefaultAccountState extension.
+pub const DEFAULT_ACCOUNT_STATE_EXTENSION: u8 = 28;
--- programs/token-2022/src/extension/mod.rs
+++ programs/token-2022/src/extension/mod.rs
@@ -1,2 +1,5 @@
 pub mod cpi_guard;
 pub mod consts;
+pub mod default_account_state;
+pub mod pausable;
+pub mod scaled_ui_amount;
