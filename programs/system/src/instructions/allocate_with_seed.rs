b1d12875265f8e963a54d86fc0a1b135f3833b45
febo <febo@anza.xyz>
2025-09-16 22:14:48 +0100

    Fix spelling

--- programs/system/src/instructions/allocate_with_seed.rs
+++ programs/system/src/instructions/allocate_with_seed.rs
@@ -23,7 +23,7 @@ pub struct AllocateWithSeed<'a, 'b, 'c> {
     /// as account 0.
     pub base: &'a AccountInfo,
 
-    /// String of ASCII chars, no longer than [MAX_SEED_LEN](https://docs.rs/solana-address/latest/solana_address/constant.MAX_SEED_LEN.html).
+    /// String of ASCII chars, no longer than [`MAX_SEED_LEN`](https://docs.rs/solana-address/latest/solana_address/constant.MAX_SEED_LEN.html).
     pub seed: &'b str,
 
     /// Number of bytes of memory to allocate.
