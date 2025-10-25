5f8281f9060e662a969f7b6b3a2629431adc9c88
LStan <LStan@users.noreply.github.com>
2025-10-26 00:07:22 +0700

    change pairing return type to bool

--- sdk/pinocchio/src/curves/bn254/pairing.rs
+++ sdk/pinocchio/src/curves/bn254/pairing.rs
@@ -8,7 +8,8 @@ pub const ALT_BN128_PAIRING_ELEMENT_SIZE: usize = ALT_BN128_G1_POINT_SIZE + ALT_
 
 const ALT_BN128_PAIRING_BE: u64 = 3;
 
-/// Perform a pairing operation on the BN254 curve in big-endian (EIP-197) encoding.
+/// Checks whether the product of pairings of a sequence of G1 and G2 points (in big-endian EIP-197 encoding)
+/// on the BN254 curve evaluates to the identity element (1).
 ///
 /// # Arguments
 ///
@@ -16,7 +17,10 @@ const ALT_BN128_PAIRING_BE: u64 = 3;
 ///
 /// # Returns
 ///
-/// A `Result` containing the result of the pairing operation, or an error if the input is invalid.
+/// A `Result` containing:
+/// - `Ok(true)` if the pairing product equals 1,
+/// - `Ok(false)` otherwise,
+/// - `Err(ProgramError)` if the input is invalid.
 ///
 /// Note: This function does **not** check if the input has the correct length.
 /// Currently, if the length is invalid, it will not return an error; instead it will use only
@@ -24,6 +28,6 @@ const ALT_BN128_PAIRING_BE: u64 = 3;
 /// After SIMD-0334 is implemented, it will return an error if the length is invalid,
 /// incurring the cost of the syscall.
 #[inline(always)]
-pub fn alt_bn128_pairing_be(input: &[u8]) -> Result<u8, ProgramError> {
-    alt_bn128_group_op::<32>(input, ALT_BN128_PAIRING_BE).map(|data| data[31])
+pub fn alt_bn128_is_pairing_valid_be(input: &[u8]) -> Result<bool, ProgramError> {
+    alt_bn128_group_op::<32>(input, ALT_BN128_PAIRING_BE).map(|data| data[31] == 1)
 }
