1d89f1377f5c6870c03762e47d56adc4ad43b0e2
cryptopapi997 <nicoschap@live.fr>
2026-01-01 00:45:57 +0800

    fmt

--- programs/token/src/lib.rs
+++ programs/token/src/lib.rs
@@ -12,15 +12,11 @@ const UNINIT_BYTE: MaybeUninit<u8> = MaybeUninit::<u8>::uninit();
 #[inline(always)]
 fn write_bytes(destination: &mut [MaybeUninit<u8>], source: &[u8]) {
     let len = destination.len().min(source.len());
-    // SAFETY: 
+    // SAFETY:
     // - Both pointers have alignment 1.
     // - For valid (non-UB) references, the borrow checker guarantees no overlap.
     // - `len` is bounded by both slice lengths.
     unsafe {
-        core::ptr::copy_nonoverlapping(
-            source.as_ptr(),
-            destination.as_mut_ptr() as *mut u8,
-            len,
-        );
+        core::ptr::copy_nonoverlapping(source.as_ptr(), destination.as_mut_ptr() as *mut u8, len);
     }
 }
