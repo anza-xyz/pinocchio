3dadd3e08db3397d9616df30fe62d09059ec0f7f
rustopian <96253492+rustopian@users.noreply.github.com>
2026-01-05 11:31:42 +0000

    use updated pinocchio stuff

--- programs/system/src/instructions/create_account_allow_prefund.rs
+++ programs/system/src/instructions/create_account_allow_prefund.rs
@@ -1,43 +1,41 @@
 use pinocchio::{
-    account_info::AccountInfo,
-    instruction::{AccountMeta, Instruction, Signer},
-    program::invoke_signed,
-    program_error::ProgramError,
-    pubkey::Pubkey,
+    cpi::{invoke_signed, Signer},
+    error::ProgramError,
+    instruction::{InstructionAccount, InstructionView},
     sysvars::rent::Rent,
-    ProgramResult,
+    AccountView, Address, ProgramResult,
 };
 
-/// Create a new account with the `lamports==0` assertion.
+/// Create a new account without the `lamports==0` assertion.
 ///
 /// ### Accounts:
 ///   0. `[WRITE, SIGNER]` New account
 ///   1. `[WRITE, SIGNER]` (OPTIONAL) Funding account
 pub struct CreateAccountAllowPrefund<'a> {
     /// Funding account and number of lamports to transfer to the new account.
-    pub payer_and_lamports: Option<(&'a AccountInfo, u64)>,
+    pub payer_and_lamports: Option<(&'a AccountView, u64)>,
 
     /// New account.
-    pub to: &'a AccountInfo,
+    pub to: &'a AccountView,
 
     /// Number of bytes of memory to allocate.
     pub space: u64,
 
     /// Address of program that will own the new account.
-    pub owner: &'a Pubkey,
+    pub owner: &'a Address,
 }
 
 impl<'a> CreateAccountAllowPrefund<'a> {
     #[inline(always)]
     pub fn with_minimal_balance(
-        from: &'a AccountInfo,
-        to: &'a AccountInfo,
-        rent_sysvar: &'a AccountInfo,
+        from: &'a AccountView,
+        to: &'a AccountView,
+        rent_sysvar: &'a AccountView,
         space: u64,
-        owner: &'a Pubkey,
+        owner: &'a Address,
     ) -> Result<Self, ProgramError> {
-        let rent = Rent::from_account_info(rent_sysvar)?;
-        let lamports = rent.minimum_balance(space as usize);
+        let rent = Rent::from_account_view(rent_sysvar)?;
+        let lamports = rent.try_minimum_balance(space as usize)?;
 
         Ok(Self {
             payer_and_lamports: Some((from, lamports)),
@@ -58,7 +56,7 @@ impl<'a> CreateAccountAllowPrefund<'a> {
         // - [0..4  ]: instruction discriminator
         // - [4..12 ]: lamports
         // - [12..20]: account space
-        // - [20..52]: owner pubkey
+        // - [20..52]: owner address
         let mut instruction_data = [0; 52];
         // create account instruction has a '0' discriminator. Lamports remains 0 here
         // but may be changed later.
@@ -67,21 +65,22 @@ impl<'a> CreateAccountAllowPrefund<'a> {
 
         if let Some((payer, lamports)) = self.payer_and_lamports {
             instruction_data[4..12].copy_from_slice(&lamports.to_le_bytes());
-            let account_metas = [
-                AccountMeta::writable_signer(self.to.key()),
-                AccountMeta::writable_signer(payer.key()),
+            let instruction_accounts: [InstructionAccount; 2] = [
+                InstructionAccount::writable_signer(self.to.address()),
+                InstructionAccount::writable_signer(payer.address()),
             ];
-            let instruction = Instruction {
+            let instruction = InstructionView {
                 program_id: &crate::ID,
-                accounts: &account_metas,
+                accounts: &instruction_accounts,
                 data: &instruction_data,
             };
             invoke_signed(&instruction, &[self.to, payer], signers)
         } else {
-            let account_metas = [AccountMeta::writable_signer(self.to.key())];
-            let instruction = Instruction {
+            let instruction_accounts: [InstructionAccount; 1] =
+                [InstructionAccount::writable_signer(self.to.address())];
+            let instruction = InstructionView {
                 program_id: &crate::ID,
-                accounts: &account_metas,
+                accounts: &instruction_accounts,
                 data: &instruction_data,
             };
             invoke_signed(&instruction, &[self.to], signers)
