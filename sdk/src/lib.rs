b567f1d5906483dc1a2411ca70ce636992be7f70
Fernando Otero <febo@anza.xyz>
2026-01-07 22:37:33 +0000

    Update sdk/src/lib.rs
    Co-authored-by: Jon C <me@jonc.dev>

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -309,7 +309,7 @@
 //! solana-define-syscall = { version = "4.0.1", features = ["unstable-static-syscalls"] }
 //! ```
 //!
-//! When compiling your program with the upstream BPF target, the `std` library is no available.
+//! When compiling your program with the upstream BPF target, the `std` library is not available.
 //! Therefore, the program crate must include the `#![no_std]` crate-level attribute and use the
 //! [`nostd_panic_handler!`] macro. An allocator may be used as long as `alloc` is used.
 
