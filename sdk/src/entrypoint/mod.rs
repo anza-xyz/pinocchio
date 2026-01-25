0be17e0f727029ed544f862ace663735db5467ea
BretasArthur1 <arthur.bretas@sou.inteli.edu.br>
2026-01-24 17:42:03 -0800

    chore: reorganize docs

--- sdk/src/entrypoint/mod.rs
+++ sdk/src/entrypoint/mod.rs
@@ -110,17 +110,17 @@ const STATIC_ACCOUNT_DATA: usize = size_of::<RuntimeAccount>() + MAX_PERMITTED_D
 /// `#[panic_handler]` set by the `std`. Therefore, this macro should be used when the program or
 /// any of its dependencies are dependent on the `std` library.
 ///
-/// When using LTO(Link Time Optimization), the compiler may inline the instruction handler (and its call tree) into the
-/// generated `entrypoint`, which can significantly increase the entrypoint stack frame. If your
-/// program has large instruction dispatch logic or builds sizeable CPI account arrays, consider
-/// adding `#[inline(never)]` to the instruction handler to keep it out of the entrypoint stack
-/// frame and avoid BPF stack overflows.
-///
 /// When the program and all its dependencies are `no_std`, it is necessary to set a
 /// `#[panic_handler]` to handle panics. This is done by the [`crate::nostd_panic_handler`] macro.
 /// In this case, it is not possible to use the `entrypoint` macro. Use the
 /// [`crate::program_entrypoint!`] macro instead and set up the allocator and panic handler
 /// manually.
+/// 
+/// The compiler may inline the instruction handler (and its call tree) into the
+/// generated `entrypoint`, which can significantly increase the entrypoint stack frame. If your
+/// program has large instruction dispatch logic or builds sizable CPI account arrays, consider
+/// adding `#[inline(never)]` to the instruction handler to keep it out of the entrypoint stack
+/// frame and avoid BPF stack overflows.
 ///
 /// [`crate::nostd_panic_handler`]: https://docs.rs/pinocchio/latest/pinocchio/macro.nostd_panic_handler.html
 #[cfg(feature = "alloc")]
