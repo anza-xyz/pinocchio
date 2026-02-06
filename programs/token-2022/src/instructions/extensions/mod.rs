667a9235ca52af4cedef209736605c1a4e62afdf
Swamikumar <nswamikumar@gmail.com>
2026-02-06 17:39:48 +0000

    confidential transfer - initialize mint instruction

--- programs/token-2022/src/instructions/extensions/mod.rs
+++ programs/token-2022/src/instructions/extensions/mod.rs
@@ -2,10 +2,12 @@ pub mod default_account_state;
 pub mod memo_transfer;
 pub mod permanent_delegate;
 pub mod transfer_hook;
+pub mod confidential_transfer;
 
 #[repr(u8)]
 #[non_exhaustive]
 pub enum ExtensionDiscriminator {
+    ConfidentialTransfer = 27,
     DefaultAccountState = 28,
     MemoTransfer = 30,
     PermanentDelegate = 35,
