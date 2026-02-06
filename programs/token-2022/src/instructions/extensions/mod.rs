fb3f0330ad223065d4d30a6ccc412ca5f8a69334
Swamikumar <nswamikumar@gmail.com>
2026-02-06 17:39:48 +0000

    confidential transfer - initialize mint instruction

--- programs/token-2022/src/instructions/extensions/mod.rs
+++ programs/token-2022/src/instructions/extensions/mod.rs
@@ -1,7 +1,9 @@
 pub mod memo_transfer;
+pub mod confidential_transfer;
 
 #[repr(u8)]
 #[non_exhaustive]
 pub enum ExtensionDiscriminator {
     MemoTransfer = 30,
+    ConfidentialTransfer = 27,
 }
