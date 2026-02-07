c2756133096fd5cace4c2c6b402aef72b0577063
Swamikumar <nswamikumar@gmail.com>
2026-02-07 19:09:52 +0000

    confidential transfer - added enable_confidential_credit insn with ci passed

--- programs/token-2022/src/instructions/extensions/confidential_transfer/instructions/mod.rs
+++ programs/token-2022/src/instructions/extensions/confidential_transfer/instructions/mod.rs
@@ -3,6 +3,7 @@ pub mod approve_account;
 pub mod configure_account;
 pub mod deposit;
 pub mod empty_account;
+pub mod enable_confidential_credits;
 pub mod initialize_mint;
 pub mod transfer;
 pub mod update_mint;
@@ -10,5 +11,6 @@ pub mod withdraw;
 
 pub use {
     apply_pending_balance::*, approve_account::*, configure_account::*, deposit::*,
-    empty_account::*, initialize_mint::*, transfer::*, update_mint::*, withdraw::*,
+    empty_account::*, enable_confidential_credits::*, initialize_mint::*, transfer::*,
+    update_mint::*, withdraw::*,
 };
