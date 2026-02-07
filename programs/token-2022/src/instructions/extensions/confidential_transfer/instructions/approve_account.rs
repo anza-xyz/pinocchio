86f6dd2d90607467bef812d4a56750df855571a3
Swamikumar <nswamikumar@gmail.com>
2026-02-07 10:06:44 +0000

    confidential transfer - approve account instruction

---
+++ programs/token-2022/src/instructions/extensions/confidential_transfer/instructions/approve_account.rs
@@ -0,0 +1,57 @@
+use solana_account_view::AccountView;
+use solana_address::Address;
+use solana_instruction_view::{
+    cpi::{invoke_signed, Signer},
+    InstructionAccount, InstructionView,
+};
+use solana_program_error::ProgramResult;
+
+/// Approves a token account for confidential transfers.
+///
+/// Approval is only required when the
+/// `ConfidentialTransferMint::approve_new_accounts` field is set in the
+/// SPL Token mint. This instruction must be executed after the account
+/// owner configures their account for credential transfers with
+/// `ConfidentialTransferInstruction::ConfigureAccount`.
+///
+/// Accounts expected by this instruction:
+///
+/// 0. `[writable]` The SPL Token account to approve.
+/// 1. `[]` The SPL Token mint.
+/// 2. `[signer]` Confidential transfer mint authority.
+pub struct ApproveAccount<'a> {
+    /// Token Account to be approved for confidential transfers
+    pub token_account: &'a AccountView,
+    /// The Token mint
+    pub mint: &'a AccountView,
+    /// Confidential Mint Authority to approve new account
+    pub authority: &'a AccountView,
+    /// The token program
+    pub token_program: &'a Address,
+}
+
+impl ApproveAccount<'_> {
+    pub fn invoke(&self) -> ProgramResult {
+        self.invoke_signed(&[])
+    }
+
+    pub fn invoke_signed(&self, signers: &[Signer]) -> ProgramResult {
+        let instruction_accounts = [
+            InstructionAccount::writable(self.token_account.address()),
+            InstructionAccount::readonly(self.mint.address()),
+            InstructionAccount::readonly_signer(self.authority.address()),
+        ];
+
+        let instruction = InstructionView {
+            program_id: self.token_program,
+            accounts: &instruction_accounts,
+            data: &[],
+        };
+
+        invoke_signed(
+            &instruction,
+            &[self.token_account, self.mint, self.authority],
+            signers,
+        )
+    }
+}
--- programs/token-2022/src/instructions/extensions/confidential_transfer/instructions/mod.rs
+++ programs/token-2022/src/instructions/extensions/confidential_transfer/instructions/mod.rs
@@ -1,5 +1,6 @@
 pub mod configure_account;
 pub mod initialize_mint;
 pub mod update_mint;
+pub mod approve_account;
 
-pub use {configure_account::*, initialize_mint::*, update_mint::*};
+pub use {configure_account::*, initialize_mint::*, update_mint::*, approve_account::*};
