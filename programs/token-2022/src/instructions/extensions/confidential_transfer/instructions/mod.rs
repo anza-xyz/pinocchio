1b5264399400c701ec54c2400f7ceb04d9cad740
Swamikumar <nswamikumar@gmail.com>
2026-02-07 14:28:28 +0000

    confidential transfer - empty_account spellcheck fix, and added deposit insn

--- programs/token-2022/src/instructions/extensions/confidential_transfer/instructions/mod.rs
+++ programs/token-2022/src/instructions/extensions/confidential_transfer/instructions/mod.rs
@@ -1,9 +1,11 @@
 pub mod approve_account;
 pub mod configure_account;
+pub mod deposit;
 pub mod empty_account;
 pub mod initialize_mint;
 pub mod update_mint;
 
 pub use {
-    approve_account::*, configure_account::*, empty_account::*, initialize_mint::*, update_mint::*,
+    approve_account::*, configure_account::*, deposit::*, empty_account::*, initialize_mint::*,
+    update_mint::*,
 };
