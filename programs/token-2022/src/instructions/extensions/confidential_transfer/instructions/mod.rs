a35da4533f1eb1cb14889cd89f014d5dc9d81a82
Swamikumar <nswamikumar@gmail.com>
2026-02-07 18:50:53 +0000

    confidential transfer - added ci passed apply_pending_balance insn

--- programs/token-2022/src/instructions/extensions/confidential_transfer/instructions/mod.rs
+++ programs/token-2022/src/instructions/extensions/confidential_transfer/instructions/mod.rs
@@ -1,3 +1,4 @@
+pub mod apply_pending_balance;
 pub mod approve_account;
 pub mod configure_account;
 pub mod deposit;
@@ -8,6 +9,6 @@ pub mod update_mint;
 pub mod withdraw;
 
 pub use {
-    approve_account::*, configure_account::*, deposit::*, empty_account::*, initialize_mint::*,
-    transfer::*, update_mint::*, withdraw::*,
+    apply_pending_balance::*, approve_account::*, configure_account::*, deposit::*,
+    empty_account::*, initialize_mint::*, transfer::*, update_mint::*, withdraw::*,
 };
