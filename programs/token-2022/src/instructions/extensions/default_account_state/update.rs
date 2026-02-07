6f9d6b4f8a522206879a4fcaee08eed8064fb132
febo <febo@anza.xyz>
2026-02-07 16:01:35 +0000

    Nits

--- programs/token-2022/src/instructions/extensions/default_account_state/update.rs
+++ programs/token-2022/src/instructions/extensions/default_account_state/update.rs
@@ -10,28 +10,34 @@ use {
     solana_program_error::{ProgramError, ProgramResult},
 };
 
-/// Update the Default-Account-State extension on a mint.
+/// Update the default state for new Accounts. Only supported for mints that
+/// include the `DefaultAccountState` extension.
 ///
-/// Expected accounts:
+/// Accounts expected by this instruction:
 ///
-/// **Single authority**
-/// 0. `[writable]` The mint account to update.
-/// 1. `[signer]` The freeze authority of the mint.
+///   * Single authority
+///   0. `[writable]` The mint.
+///   1. `[signer]` The mint freeze authority.
 ///
-/// **Multisignature authority**
-/// 0. `[writable]` The mint account to update.
-/// 1. `[readonly]` The multisig freeze authority.
-/// 2. `[signer]` M signer accounts (as required by the multisig).
+///   * Multisignature authority
+///   0. `[writable]` The mint.
+///   1. `[]` The mint's multisignature freeze authority.
+///   2. `..2+M` `[signer]` M signer accounts.
 pub struct Update<'a, 'b, 'c> {
-    /// The mint account to update.
-    pub mint_account: &'a AccountView,
-    /// The freeze authority of the mint (single or multisig).
+    /// The mint.
+    pub mint: &'a AccountView,
+
+    /// The mint freeze authority.
     pub freeze_authority: &'a AccountView,
-    /// Signer accounts if the authority is a multisig.
+
+    /// The signer accounts if the authority is a multisig.
     pub signers: &'c [&'a AccountView],
-    /// The default account state in which new token accounts should be initialized.
+
+    /// The new account state in which new token accounts should be
+    /// initialized.
     pub state: u8,
-    /// Token program (Token-2022).
+
+    /// The token program.
     pub token_program: &'b Address,
 }
 
@@ -45,75 +51,82 @@ impl Update<'_, '_, '_> {
 
     #[inline(always)]
     pub fn invoke_signed(&self, signers: &[Signer]) -> ProgramResult {
-        let &Self {
-            mint_account,
-            freeze_authority,
-            signers: multisig_accounts,
-            token_program,
-            ..
-        } = self;
-
-        if multisig_accounts.len() > MAX_MULTISIG_SIGNERS {
+        if self.signers.len() > MAX_MULTISIG_SIGNERS {
             return Err(ProgramError::InvalidArgument);
         }
 
+        let expected_accounts = 2 + self.signers.len();
+
+        // Instruction accounts.
+
         const UNINIT_INSTRUCTION_ACCOUNTS: MaybeUninit<InstructionAccount> =
             MaybeUninit::<InstructionAccount>::uninit();
-        let mut accounts = [UNINIT_INSTRUCTION_ACCOUNTS; 2 + MAX_MULTISIG_SIGNERS];
+        let mut instruction_accounts = [UNINIT_INSTRUCTION_ACCOUNTS; 2 + MAX_MULTISIG_SIGNERS];
 
-        // SAFETY:
-        // - `account_metas` is sized to 2 + MAX_MULTISIG_SIGNERS
-        // Index 0 and 1 are always present
+        // SAFETY: The allocation is valid to the maximum number of accounts.
         unsafe {
-            accounts
+            // mint
+            instruction_accounts
                 .get_unchecked_mut(0)
-                .write(InstructionAccount::writable(mint_account.address()));
+                .write(InstructionAccount::writable(self.mint.address()));
 
-            accounts.get_unchecked_mut(1).write(InstructionAccount::new(
-                freeze_authority.address(),
-                false,
-                multisig_accounts.is_empty(),
-            ));
+            // freeze_authority
+            instruction_accounts
+                .get_unchecked_mut(1)
+                .write(InstructionAccount::new(
+                    self.freeze_authority.address(),
+                    false,
+                    self.signers.is_empty(),
+                ));
+
+            // signer accounts
+            for (account, signer) in instruction_accounts
+                .get_unchecked_mut(2..)
+                .iter_mut()
+                .zip(self.signers.iter())
+            {
+                account.write(InstructionAccount::readonly_signer(signer.address()));
+            }
         }
-
-        for (account, signer) in accounts[2..].iter_mut().zip(multisig_accounts.iter()) {
-            account.write(InstructionAccount::readonly_signer(signer.address()));
-        }
-
-        let data = &[
-            ExtensionDiscriminator::DefaultAccountState as u8,
-            Self::DISCRIMINATOR,
-            self.state,
-        ];
-
-        let num_accounts = 2 + multisig_accounts.len();
 
         let instruction = InstructionView {
-            program_id: token_program,
-            data,
-            accounts: unsafe { slice::from_raw_parts(accounts.as_ptr() as _, num_accounts) },
+            program_id: self.token_program,
+            data: &[
+                ExtensionDiscriminator::DefaultAccountState as u8,
+                Self::DISCRIMINATOR,
+                self.state,
+            ],
+            accounts: unsafe {
+                slice::from_raw_parts(instruction_accounts.as_ptr() as _, expected_accounts)
+            },
         };
 
-        // Account view array
+        // Accounts.
+
         const UNINIT_ACCOUNT_VIEWS: MaybeUninit<&AccountView> = MaybeUninit::uninit();
-        let mut account_views = [UNINIT_ACCOUNT_VIEWS; 2 + MAX_MULTISIG_SIGNERS];
+        let mut accounts = [UNINIT_ACCOUNT_VIEWS; 2 + MAX_MULTISIG_SIGNERS];
 
-        // SAFETY:
-        // - `account_views` is sized to 2 + MAX_MULTISIG_SIGNERS
-        // Index 0 and 1 are always present
+        // SAFETY: The allocation is valid to the maximum number of accounts.
         unsafe {
-            account_views.get_unchecked_mut(0).write(mint_account);
-            account_views.get_unchecked_mut(1).write(freeze_authority);
-        }
+            // mint
+            accounts.get_unchecked_mut(0).write(self.mint);
 
-        // Fill signer accounts
-        for (account_view, signer) in account_views[2..].iter_mut().zip(multisig_accounts.iter()) {
-            account_view.write(signer);
+            // freeze_authority
+            accounts.get_unchecked_mut(1).write(self.freeze_authority);
+
+            // signer accounts
+            for (account, signer) in accounts
+                .get_unchecked_mut(2..)
+                .iter_mut()
+                .zip(self.signers.iter())
+            {
+                account.write(signer);
+            }
         }
 
         invoke_signed_with_bounds::<{ 2 + MAX_MULTISIG_SIGNERS }>(
             &instruction,
-            unsafe { slice::from_raw_parts(account_views.as_ptr() as _, num_accounts) },
+            unsafe { slice::from_raw_parts(accounts.as_ptr() as _, expected_accounts) },
             signers,
         )
     }
