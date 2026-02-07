b549200621a826b6228ffcd4652d6fb38fd51f0b
Swamikumar <nswamikumar@gmail.com>
2026-02-07 18:08:21 +0000

    confidential transfers - added transfer instruction, and ci script passed

--- programs/token-2022/src/instructions/extensions/confidential_transfer/instructions/mod.rs
+++ programs/token-2022/src/instructions/extensions/confidential_transfer/instructions/mod.rs
@@ -3,10 +3,11 @@ pub mod configure_account;
 pub mod deposit;
 pub mod empty_account;
 pub mod initialize_mint;
+pub mod transfer;
 pub mod update_mint;
 pub mod withdraw;
 
 pub use {
     approve_account::*, configure_account::*, deposit::*, empty_account::*, initialize_mint::*,
-    update_mint::*, withdraw::*,
+    transfer::*, update_mint::*, withdraw::*,
 };
