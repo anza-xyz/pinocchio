4a4158d8e0fdeeccb430f42733bff2716e7841f6
febo <febo@anza.xyz>
2026-02-08 18:14:02 +0000

    Add extension instructions

--- programs/token-2022/src/instructions/extensions/mod.rs
+++ programs/token-2022/src/instructions/extensions/mod.rs
@@ -1,6 +1,7 @@
 pub mod default_account_state;
 pub mod group_member_pointer;
 pub mod group_pointer;
+pub mod interest_bearing_mint;
 pub mod memo_transfer;
 pub mod metadata_pointer;
 pub mod permanent_delegate;
@@ -13,6 +14,7 @@ pub mod transfer_hook;
 pub enum ExtensionDiscriminator {
     DefaultAccountState = 28,
     MemoTransfer = 30,
+    InterestBearingMint = 33,
     PermanentDelegate = 35,
     TransferHook = 36,
     MetadataPointer = 39,
