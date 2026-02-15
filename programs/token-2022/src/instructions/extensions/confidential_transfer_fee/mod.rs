072b5146c451507308750a6619b15759ea31db62
Swamikumar <nswamikumar@gmail.com>
2026-02-15 02:39:33 +0000

    added enable_harvest_to_mint instruction

--- programs/token-2022/src/instructions/extensions/confidential_transfer_fee/mod.rs
+++ programs/token-2022/src/instructions/extensions/confidential_transfer_fee/mod.rs
@@ -1,9 +1,11 @@
+pub mod enable_harvest_to_mint;
 pub mod harvest_withheld_tokens_to_mint;
 pub mod initialize_confidential_tranfer_fee_config;
 pub mod withdraw_withheld_tokens_from_accounts;
 pub mod withdraw_withheld_tokens_from_mint;
 
 pub use {
-    harvest_withheld_tokens_to_mint::*, initialize_confidential_tranfer_fee_config::*,
-    withdraw_withheld_tokens_from_accounts::*, withdraw_withheld_tokens_from_mint::*,
+    enable_harvest_to_mint::*, harvest_withheld_tokens_to_mint::*,
+    initialize_confidential_tranfer_fee_config::*, withdraw_withheld_tokens_from_accounts::*,
+    withdraw_withheld_tokens_from_mint::*,
 };
