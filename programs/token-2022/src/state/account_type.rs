32ee84390d61936e55ceb4d79d468ce036a162ff
febo <febo@anza.xyz>
2026-04-08 23:36:25 +0300

    Remove unused code

--- programs/token-2022/src/state/account_type.rs
+++ programs/token-2022/src/state/account_type.rs
@@ -1,5 +1,3 @@
-use crate::state::{Account, Mint};
-
 /// Different kinds of accounts. Note that `Mint`, `Account`, and `Multisig`
 /// types are determined exclusively by the size of the account, and are not
 /// included in the account data. `AccountType` is only included if extensions
@@ -13,34 +11,4 @@ pub enum AccountType {
     Mint,
     /// Token holding account with additional extensions
     Account,
-}
-
-impl AccountType {
-    /// Return the expected length of an account for the given `AccountType`.
-    pub const fn len_for_type(&self) -> usize {
-        match self {
-            AccountType::Uninitialized => 0,
-            AccountType::Mint => Mint::BASE_LEN,
-            AccountType::Account => Account::BASE_LEN,
-        }
-    }
-}
-
-impl From<u8> for AccountType {
-    fn from(value: u8) -> Self {
-        match value {
-            0..=2 => unsafe { core::mem::transmute::<u8, AccountType>(value) },
-            _ => panic!("invalid account type value: {value}"),
-        }
-    }
-}
-
-impl From<AccountType> for u8 {
-    fn from(value: AccountType) -> Self {
-        match value {
-            AccountType::Uninitialized => 0,
-            AccountType::Mint => 1,
-            AccountType::Account => 2,
-        }
-    }
 }
